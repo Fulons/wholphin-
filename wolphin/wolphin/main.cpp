81888dd0ab6ec862b415d6c8b404a84f71299262
Fulons <fulons.k@gmail.com>
2016-10-14 21:11:07 +0100

    Test dif commit

--- wolphin/wolphin/main.cpp
+++ wolphin/wolphin/main.cpp
@@ -2,9 +2,16 @@
 #define THIS_IS_THE_START_OF_OUR_GAME_GUYS 0x13333337
 #include <iostream>
 
+class Game {
+	bool theGameIsRunning = false;
+public:
+	void RunThisAwesomeGame() { theGameIsRunning = true; }
+};
+
 void main() {
+	Game game;
 	std::cout << "Wolphins are awsome" << std::endl;
+	game.RunThisAwesomeGame();
 	system("PAUSE");
 }
-
 
